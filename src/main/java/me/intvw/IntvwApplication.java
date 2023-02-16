041ab8f2d1667beec0c50c33b12290273c441f42
Oleksandr <oleksandr.pavliuk@hevelian.com>
2023-02-16 11:01:17 +0200

    Initial commit

---
+++ src/main/java/me/intvw/IntvwApplication.java
@@ -0,0 +1,13 @@
+package me.intvw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntvwApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IntvwApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/intvw/IntvwApplicationTests.java
@@ -0,0 +1,13 @@
+package me.intvw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntvwApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
